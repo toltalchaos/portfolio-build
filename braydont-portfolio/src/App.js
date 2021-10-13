1e517d1f340d3f29ba9c8accfc6c89d0174cc25f
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-10-13 07:35:01 -0600

    add routing

--- braydont-portfolio/src/App.js
+++ braydont-portfolio/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import {Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
   return (
-    <p>REACTAPP</p>
+    <Router>
+      {/* header+navdeck */}
+      <Switch>
+        <Route path="/">
+          {/* homepage */}
+        </Route>
+        <Route path="/git">
+          {/* githubpage */}
+        </Route>
+        <Route path="/links">
+          {/* links */}
+        </Route>
+        <Route path="/about">
+          {/* about/hobbies page */}
+        </Route>
+        <Route path="/contact">
+          {/* contact page */}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
